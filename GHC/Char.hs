a0548a5498528b25b16355826d4fe08112f3f49d
Roman Cheplyaka <roma@ro-che.info>
2013-02-22 16:27:01 +0200

    MagicHash bug

--- GHC/Char.hs
+++ GHC/Char.hs
@@ -5,11 +5,3 @@ module GHC.Char (chr) where
 
 import GHC.Base
 import GHC.Show
-
--- | The 'Prelude.toEnum' method restricted to the type 'Data.Char.Char'.
-chr :: Int -> Char
-chr i@(I# i#)
- | int2Word# i# `leWord#` 0x10FFFF## = C# (chr# i#)
- | otherwise
-    = error ("Prelude.chr: bad argument: " ++ showSignedInt (I# 9#) i "")
-
